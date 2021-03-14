71fcd3379003affcf4833ac2c73d491bc6f61f58
MaryColasanto <colasanto.mary@gmail.com>
2021-03-13 21:48:46 -0600

    Replace sql file with finel database format

--- NBA_Schemata.sql
+++ NBA_Schemata.sql
@@ -1,62 +1,54 @@
-CREATE TABLE "player" (
+CREATE TABLE "demographics" (
     "player_id" VARCHAR   NOT NULL,
-    "name" VARCHAR,
-    CONSTRAINT "pk_player" PRIMARY KEY (
+    "name" VARCHAR   NOT NULL,
+    "height" NUMERIC   NOT NULL,
+    "weight" INT   NOT NULL,
+    "birth_year" INT   NOT NULL,
+    "birth_city" VARCHAR   NOT NULL,
+    "birth_state" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_demographics" PRIMARY KEY (
         "player_id"
      )
 );
 
-CREATE TABLE "demographics" (
-    "player_id" VARCHAR NOT NULL,
-    "height" NUMERIC,
-    "weight" INT,
-    "birth_year" INT,
-    "birth_city" VARCHAR,
-    "birth_state" VARCHAR
-);
-
 CREATE TABLE "salary" (
-    "player_id" VARCHAR NOT NULL,
-    "salary" INT,
-    "season" INT,
-    "team" VARCHAR
+    "player_id" VARCHAR   NOT NULL,
+    "salary" INT   NOT NULL,
+    "season" INT   NOT NULL,
+    "team" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_salary" PRIMARY KEY (
+        "player_id"
+     )
 );
 
 CREATE TABLE "position" (
-    "player_id" VARCHAR,
-    "position" VARCHAR,
-    "shoots" VARCHAR
+    "player_id" VARCHAR   NOT NULL,
+    "position" VARCHAR   NOT NULL,
+    "shoots" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_position" PRIMARY KEY (
+        "player_id"
+     )
 );
 
 CREATE TABLE "draft" (
-    "player_id" VARCHAR NOT NULL,
-    "draft_pick" VARCHAR,
-    "draft_round" VARCHAR,
-    "draft_team" VARCHAR,
-    "draft_year" INT
+    "player_id" VARCHAR   NOT NULL,
+    "draft_pick" VARCHAR   NOT NULL,
+    "draft_round" VARCHAR   NOT NULL,
+    "draft_team" VARCHAR   NOT NULL,
+    "draft_year" INT   NOT NULL,
+    CONSTRAINT "pk_draft" PRIMARY KEY (
+        "player_id"
+     )
 );
 
 CREATE TABLE "schools" (
-    "player_id" VARCHAR NOT NULL,
-    "high_school" VARCHAR,
-    "hs_city" VARCHAR,
-    "hs_state" VARCHAR,
-    "college" VARCHAR,
-    "college_2" VARCHAR
+    "player_id" VARCHAR   NOT NULL,
+    "high_school" VARCHAR   NOT NULL,
+    "hs_city" VARCHAR   NOT NULL,
+    "hs_state" VARCHAR   NOT NULL,
+    "college" VARCHAR   NOT NULL,
+    "college_2" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_schools" PRIMARY KEY (
+        "player_id"
+     )
 );
-
-ALTER TABLE "demographics" ADD CONSTRAINT "fk_demographics_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("player_id");
-
-ALTER TABLE "salary" ADD CONSTRAINT "fk_salary_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("player_id");
-
-ALTER TABLE "position" ADD CONSTRAINT "fk_position_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("player_id");
-
-ALTER TABLE "draft" ADD CONSTRAINT "fk_draft_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("player_id");
-
-ALTER TABLE "schools" ADD CONSTRAINT "fk_schools_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("player_id");
-
